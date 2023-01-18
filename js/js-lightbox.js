914f1481fb5da8e552fdb592b4d9515e74b49227
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-01-18 22:53:06 +0200

    cool

---
+++ js/js-lightbox.js
@@ -0,0 +1,15 @@
+const gallery = document.querySelector(".gallery");
+
+let markup = '';
+
+for (let i = 1; i < 73; i += 1){
+  markup += `<a class="gallery__item" href="./img/${i}.jpg">
+      <img class="gallery__image" src="./img/${i}.jpg" alt="" /></a>`;
+}
+
+gallery.innerHTML = markup;
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
