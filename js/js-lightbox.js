b069430d67d3c833fa7aa5f8b54c34ce3d838f4e
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-01-20 19:25:32 +0200

    commented out the extra

--- js/js-lightbox.js
+++ js/js-lightbox.js
@@ -4,12 +4,12 @@ let markup = '';
 
 for (let i = 1; i < 73; i += 1){
   markup += `<a class="gallery__item" href="./img/${i}.jpg">
-      <img class="gallery__image" src="./img/${i}.jpg" alt="" /></a>`;
+      <img class="gallery__image" src="./img/${i}.jpg" loading="lazy" alt="" /></a>`;
 }
 
 gallery.innerHTML = markup;
 
 new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
+  // captionsData: "alt",
+  // captionDelay: 250,
 });
